092f0beb6e0176d1c716b6fa13c9503e4f1976c5
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-10-03 18:09:48 +0200

    Fixing "Create your first project" button

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -110,7 +110,7 @@ export default function Dashboard() {
                     <div className={styles.noProjectContainer}>
                         <Image alt='No projects' layout='fill' src="/illustrations/no-project.svg" />
                     </div>
-                    <button className='button round nq-button blue center' onClick={() => props.setMenu(<CreateProjectMenu setMenu={props.setMenu} />, document)}>
+                    <button className='button round nq-button blue center' onClick={() => setMenu(<CreateProjectMenu setMenu={setMenu} />, document)}>
                     <div className='flex'>
                         <svg fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
